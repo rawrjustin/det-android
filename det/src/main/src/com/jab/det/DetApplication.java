a5c6dd4d94259c7fed342cabd30dbc106f33e30c
Allen <wua05@ucla.edu>
2013-10-18 04:40:24 -0700

    Added facebook integration and login/logout

--- det/src/main/src/com/jab/det/DetApplication.java
+++ det/src/main/src/com/jab/det/DetApplication.java
@@ -1,15 +1,20 @@
 package com.jab.det;
 
 import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+
 import android.app.Application;
 
 public class DetApplication extends Application {
 
+	static final String TAG = "DetApp";
 	@Override
 	public void onCreate() {
 		// Initialize Parse
-		Parse.initialize(this, "SnZIklEaMlecec0zVwMgEgXDgeDWgM458xCUXPqU", "TLNJj6cz7SquDWwPlNHRg79YGHWpftHbh0WoEyLx"); 
-
+		Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key)); 
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+		
+		// Call superclass's onCreate method
 		super.onCreate();
 	}
 
---
+++ det/src/main/src/com/jab/det/UserHomeActivity.java
@@ -0,0 +1,72 @@
+package com.jab.det;
+
+import com.parse.ParseUser;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class UserHomeActivity extends Activity {
+
+	private Button logoutButton;
+	private TextView userIntroView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+        
+        userIntroView = (TextView) findViewById(R.id.user_home_intro);
+        
+    	logoutButton = (Button) findViewById(R.id.logoutButton);
+		logoutButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onLogoutButtonClicked();
+			}
+		});
+    }
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			userIntroView.setText("Hi user " + currentUser.getUsername());
+			// Pull user's transactions and display them
+			Log.d(DetApplication.TAG, "Failed to retrieve current user on UserHomeActivity resume");
+		} else {
+			// If the user is not logged in, go to the
+			// activity showing the login view.
+			startLoginActivity();
+		}
+	}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.user_home, menu);
+        return true;
+    }
+    
+	private void onLogoutButtonClicked() {
+		// Log the user out
+		ParseUser.logOut();
+
+		// Go to the login view
+		startLoginActivity();
+	}
+
+	private void startLoginActivity() {
+		Intent intent = new Intent(this, LoginActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+	}
+}
