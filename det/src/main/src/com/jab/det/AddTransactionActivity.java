600881d5a0de3083ca69a97ee75bb06bd1e8e009
Allen <wua05@ucla.edu>
2013-10-25 02:31:01 -0700

    Layout changes

--- det/src/main/src/com/jab/det/AddTransactionActivity.java
+++ det/src/main/src/com/jab/det/AddTransactionActivity.java
@@ -60,10 +60,6 @@ public class AddTransactionActivity extends Activity {
             	onClickSubmitTransaction();
             }
         });
-        
-        //add_transaction_submit
-        // Show the Up button in the action bar.
-        setupActionBar();
     }
 
     @Override
@@ -180,45 +176,10 @@ public class AddTransactionActivity extends Activity {
 			startActivity(intent);
 		}
     
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private void setupActionBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_transaction, menu);
-        return true;
-    }
-    
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
     @Override
     public void onDestroy(){
         super.onDestroy();
-        if (progressDialog !=null && progressDialog.isShowing()){
+        if (progressDialog != null && progressDialog.isShowing()){
             progressDialog.cancel();
         }
     }
