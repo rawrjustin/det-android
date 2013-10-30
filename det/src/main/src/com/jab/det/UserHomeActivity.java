299abc0154fea46e9c168fd5b912d1290774bfd5
Allen <wua05@ucla.edu>
2013-10-30 03:51:07 -0700

    Added loading and refresh

--- det/src/main/src/com/jab/det/UserHomeActivity.java
+++ det/src/main/src/com/jab/det/UserHomeActivity.java
@@ -21,35 +21,44 @@ import android.widget.TextView;
 public class UserHomeActivity extends Activity {
 
 	private Button logoutButton;
+	private Button refreshButton;
+	private Button addTransactionButton;
 	private TextView userIntroView;
 	private ListView debtListView;
-	private DisplayDebtsAdapter debtListAdapter;
 	private static DTUser currentUser;
+	private LoadDebtsData loadDebtsData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
+        this.debtListView = (ListView) findViewById(R.id.debt_list);
         setCurrentUser();
+        setupAddTransactionButton();
+        setupRefreshButton();
 		setupLogoutButton();
 		displayDebts();
-		// Set loading message
-		//debtListView = (ListView) findViewById(R.id.debt_list);
-		//debtListAdapter = new ArrayAdapter<String>(this, R.layout.debt_row, new ArrayList<String>(Arrays.asList("Loading debts...")));
-		//debtListView.setAdapter(debtListAdapter);
-		/*Thread thread = new Thread() {
-			public void run() {
-				displayDebts();
-			}
-		};
-		
-		thread.start();*/
     }
-
+   
+    private void setupRefreshButton() {
+    	this.refreshButton = (Button) findViewById(R.id.refreshDebtsButton);
+		this.refreshButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onRefreshButtonClicked();
+			}
+		});
+    }
+    
+	private void onRefreshButtonClicked() {
+		this.loadDebtsData = new LoadDebtsData(this, getWindow().getDecorView().getRootView());
+		this.loadDebtsData.execute();
+	}
+    
     // Adds onClick listener for logout button
     private void setupLogoutButton() {
-    	logoutButton = (Button) findViewById(R.id.logoutButton);
-		logoutButton.setOnClickListener(new View.OnClickListener() {
+    	this.logoutButton = (Button) findViewById(R.id.logoutButton);
+		this.logoutButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				onLogoutButtonClicked();
@@ -76,23 +85,12 @@ public class UserHomeActivity extends Activity {
     // Gets all of current user's associated debts and writes them to the ListView
     private void displayDebts() {
 		// Populate debtsList ListView with debts
-		debtListView = (ListView) findViewById(R.id.debt_list);
-		
-		DTDebt[] currentUserDebts = currentUser.getDebts();
-
-		if (currentUserDebts.length == 0) {
-			debtListView.setAdapter(new ArrayAdapter<String>(this, R.layout.simple_debt_row, 
-					new ArrayList<String>(Arrays.asList("You are not a part of any debts"))));
-		} else {
-			// Writes debts to view
-			debtListAdapter = new DisplayDebtsAdapter(this, R.layout.debt_row, currentUserDebts);
-			debtListView.setAdapter(debtListAdapter);
-		}
+		this.loadDebtsData = new LoadDebtsData(this, getWindow().getDecorView().getRootView());
+		loadDebtsData.execute();
     }
 
     // Called when user clicks add transaction button
-	public void addTransaction(View view) {
-    	Log.d(DetApplication.TAG, "Add transaction button clicked");
+	private void onAddTransactionButtonClicked() {
     	AddTransactionActivity.setSelectedUsers(null);
     	startAddTransactionActivity();
     }
@@ -102,7 +100,17 @@ public class UserHomeActivity extends Activity {
 		Intent intent = new Intent(this, AddTransactionActivity.class);
 		startActivity(intent);
 	}
-	
+
+    private void setupAddTransactionButton() {
+    	this.addTransactionButton = (Button) findViewById(R.id.addTransactionButton);
+    	this.addTransactionButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onAddTransactionButtonClicked();
+			}
+		});
+    }
+    
 	// Logs the user out and starts LoginActivity
 	private void onLogoutButtonClicked() {
 		// Log the user out
