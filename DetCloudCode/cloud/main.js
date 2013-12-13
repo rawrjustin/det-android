d616b3ca457b7bab7529c001e556aebe932322bc
Justin Huang <justingotemail@gmail.com>
2013-12-13 03:01:59 -0800

    cloud code modification for owe/paid

--- DetCloudCode/cloud/main.js
+++ DetCloudCode/cloud/main.js
@@ -29,7 +29,7 @@ Parse.Cloud.define("createTransaction", function(request, response) {
 	
 	// initialize an array to hold all created debts
 	var savedDebts = new Object();
-
+	
 	// initialize a counter of debts to save
 	var debtsLeftToSave = facebookIds.length;
 
@@ -48,14 +48,15 @@ Parse.Cloud.define("createTransaction", function(request, response) {
 
 		//get the initial creditor user via query
 		var creditorQuery = new Parse.Query(ParseUsers);
-		creditorQuery.get(request.params["creditor"], {
-		  success: function(creditor) {
-		    // The creditor was retrieved successfully.
+		creditorQuery.get(request.params["user"], {
+		  success: function(myUser) {
+		    // The user was retrieved successfully.
 		    // Try to find users already in DB
 			var existingQuery = new Parse.Query(ParseUsers);
 			existingQuery.containedIn("fbID", facebookIds);
 		    existingQuery.find({
 		  	success: function(results) {
+
 		  		// found users that exist in DB
 			    if (results.length > 0) {
 			    	// for each existing user, create debt, and remove their ID from facebookIDs
@@ -65,14 +66,21 @@ Parse.Cloud.define("createTransaction", function(request, response) {
 			    		//create debt object for this existing user
   						var debt = new Debt();
   						var userFbID = existingUser.get("fbID"); 
-  
-  						debt.set("debtor", existingUser);
-  						debt.set("creditor", creditor);
+              var amount = request.params[userFbID]["amount"];
+              if (amount > 0) {
+                debt.set("debtor", existingUser);
+                debt.set("creditor", myUser);
+              } else {
+                debt.set("debtor", myUser);
+                debt.set("creditor", existingUser);
+              }
+  						
   						debt.set("amount", request.params[userFbID]["amount"]);
   						debt.set("transaction", transaction);
   						debt.save(null, {
   						  success: function(debt) {
-  						    savedDebts[userFbID] = debt;
+  						    var pulledFBID = debt.get("debtor").get("fbID");
+  						    savedDebts[pulledFBID] = debt;
   						  	debtsLeftToSave--;
   						  	if (debtsLeftToSave == 0) {
   						  		savedDebts["transaction"] = transaction;
@@ -94,6 +102,10 @@ Parse.Cloud.define("createTransaction", function(request, response) {
 			    // we now have the number of users left to signup in faecbookIds. 
 			    // for each user to create, signup user. 
 		    	var numToCreate = facebookIds.length;
+		    	
+		    	//make an array for users after signup to debit
+		    	var usersToCreateDebts = new Array();
+		    	
 		    	for (var i = 0; i < facebookIds.length; i++) {
 		    		var fbID = facebookIds[i];
 		    		var user = new Parse.User();
@@ -105,20 +117,28 @@ Parse.Cloud.define("createTransaction", function(request, response) {
   					 
   					user.signUp(null, {
   					  success: function(user) {
+  					    usersToCreateDebts.push(user);
   					  	numToCreate--;
   					  	if (numToCreate == 0) {
-  							 
-  					  		for (var i = 0; i < facebookIds.length; i++) {
+  					  		for (var i = 0; i < usersToCreateDebts.length; i++) {
     								var debt = new Debt();
-    								var userFbID = user.get("fbID");
-    
-    								debt.set("debtor", user);
-    								debt.set("creditor", creditor);
+    								var newUser = usersToCreateDebts[i];
+    								var userFbID = newUser.get("fbID");
+    								
+    								var amount = request.params[userFbID]["amount"];
+                    if (amount > 0) {
+                      debt.set("debtor", newUser);
+                      debt.set("creditor", myUser);
+                    } else {
+                      debt.set("debtor", myUser);
+                      debt.set("creditor", newUser);
+                    }
     								debt.set("amount", request.params[userFbID]["amount"]);
     								debt.set("transaction", transaction);
     								debt.save(null, {
     								  success: function(debt) {
-                        savedDebts[userFbID] = debt;
+    								    var pulledFBID = debt.get("debtor").get("fbID");
+                        savedDebts[pulledFBID] = debt;
     								  	debtsLeftToSave--;
     								  	if (debtsLeftToSave == 0) {
     								  	  savedDebts["transaction"] = transaction;
@@ -147,7 +167,7 @@ Parse.Cloud.define("createTransaction", function(request, response) {
 		  error: function(object, error) {
 		    // The object was not retrieved successfully.
 		    // error is a Parse.Error with an error code and description.
-		    response.error("Cant find creditor: " + request.params["creditor"] + error.code + " " + error.message);
+		    response.error("Cant find creditor: " + request.params["user"] + error.code + " " + error.message);
 		  }
 		});
 	  },
